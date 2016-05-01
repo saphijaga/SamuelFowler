e26b7652dc995a7f24064ae067631fa3e09ef14d
samuel <samuel.philipp@t-online.de>
2016-05-01 20:44:00 +0200

    Replace Conditional with Polymorphism

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -9,4 +9,11 @@ public class NewReleasePrice extends Price {
     public double getCharge(int daysRented) {
         return (daysRented * 3);
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if (daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
