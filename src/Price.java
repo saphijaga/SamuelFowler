10c111d70f70539084bf7f9bb8f4e6e9d17dd3fb
samuel <samuel.philipp@t-online.de>
2016-05-01 20:36:53 +0200

    Changing PriceCode to Price in Movie - Move Method

--- src/Price.java
+++ src/Price.java
@@ -2,5 +2,25 @@
  * Created by samuel on 01.05.16.
  */
 public abstract class Price {
-    abstract int getPriceCode();
+    public abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double charge = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                charge += 1.5;
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+                break;
+        }
+        return charge;
+    }
 }
